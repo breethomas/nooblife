4562e48d12e665b270f2da0012a0c78380b1da7e
Bree Thomas <bree@aha-method.com>
2014-01-29 12:21:54 -0700

    Metaprogramming post

--- config.rb
+++ config.rb
@@ -64,10 +64,12 @@ page "/feed.xml", :layout => false
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true
+activate :syntax, line_numbers: true
+
 
 # Build-specific configuration
 configure :build do
